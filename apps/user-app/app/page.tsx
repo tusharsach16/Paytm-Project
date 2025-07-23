1a987bfe2c93bd9df92ad09af9db925669e5c36b
Tushar <tusharschdeva16@gmail.com>
2025-07-24 02:29:21 +0530

    Added the db

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
 export default function Home() {
   return (
     <div className="bg-red-300 flex flex-col justify-center items-center min-h-screen text-2xl">
