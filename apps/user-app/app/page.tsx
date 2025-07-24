a570cdfd2a3baa3d1b30b1fc88f8e7ae2753e89c
Tushar <tusharschdeva16@gmail.com>
2025-07-25 02:53:20 +0530

    Update Schema

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-red-300 flex flex-col justify-center items-center min-h-screen text-2xl">
       <div className="bg-blue-300">
-        Hi Tushar
+        Paytm
       </div>
     </div>
   );
